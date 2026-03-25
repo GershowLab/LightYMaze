59acf9c79e1533cce72473b6f650a025fe02b608
mgershow <gershow@gmail.com>
2026-03-25 11:03:05 -0400

    added express set up to livetracker
    reordered loop

--- livetracker.py
+++ livetracker.py
@@ -11,68 +11,77 @@ from cameracapture import CameraCapture
 print("boot")
 cap = CameraCapture()
 
-print("cam cap")
-cv2.namedWindow('focus - c to continue', cv2.WINDOW_NORMAL)
-
-print("focus")
-while True: #should be True, changed to speed up testing
-	#try:
-	im,ts = cap.capture_frame()
-	cv2.imshow('focus - c to continue', im)
-	key = cv2.waitKey(1) & 0xFF
-	if key == ord('c'):
-		break
-#except:
-	#	print("error")
-cv2.destroyAllWindows()
-
-
-ymg = YMazeGeometry()
-ymg.set_image_size((cap.h, cap.w))
-while True:
-	cv2.namedWindow('mazes', cv2.WINDOW_KEEPRATIO)
-	im,ts = cap.capture_frame()
-	img = ymg.calibrate_geometry_from_image(im)
-#	cv2.imshow('mazes', im)
-#	cv2.waitKey(1)
-#	ymg.two_point_rotation_and_scaling((1763,1349), (1055,2116))
-#	img = ymg.diagnostic_image(im.copy())
-
-	# im,ts = cap.capture_frame()
-	cv2.imshow('mazes', img)
-	cv2.waitKey(1)
-	x,y,w,h = ymg.clip_to_mazes(10)
-	cap.cam.stop()
-	cap.set_bounding_box(x,y,w,h)
+express = True
+c = (1764,1345)
+m4 = (1053, 2101)
+if express:
+	print("express setup")
+	ymg = YMazeGeometry()
+	ymg.set_image_size((cap.h, cap.w))
+	ymg.two_point_rotation_and_scaling(c, m4)
+	x, y, w, h = ymg.clip_to_mazes(10)
+	cap.set_bounding_box(x, y, w, h)
 	cap.cam.start()
-	im,ts = cap.capture_frame()
-	cv2.namedWindow('clipped mazes', cv2.WINDOW_NORMAL)
-	img = ymg.diagnostic_image(im)
-	cv2.imshow('clipped mazes', img)
+else:
+	print("cam cap")
+	cv2.namedWindow('focus - c to continue', cv2.WINDOW_NORMAL)
+
+	print("focus")
+	while True: #should be True, changed to speed up testing
+		#try:
+		im,ts = cap.capture_frame()
+		cv2.imshow('focus - c to continue', im)
+		key = cv2.waitKey(1) & 0xFF
+		if key == ord('c'):
+			break
+	#except:
+		#	print("error")
+	cv2.destroyAllWindows()
 
 
-	key = cv2.waitKey(1) & 0xFF
-	if key == ord('q'):
-		quit()
-	response = input("Are you satisfied with the maze locations? (yes/no)")
-	if response == "yes":
-		break
-#cv2.destroyAllWindows()
+	ymg = YMazeGeometry()
+	ymg.set_image_size((cap.h, cap.w))
+	while True:
+		cv2.namedWindow('mazes', cv2.WINDOW_KEEPRATIO)
+		im,ts = cap.capture_frame()
+		img = ymg.calibrate_geometry_from_image(im)
+	#	cv2.imshow('mazes', im)
+	#	cv2.waitKey(1)
+	#	ymg.two_point_rotation_and_scaling((1763,1349), (1055,2116))
+	#	img = ymg.diagnostic_image(im.copy())
 
+		# im,ts = cap.capture_frame()
+		cv2.imshow('mazes', img)
+		cv2.waitKey(1)
+		x,y,w,h = ymg.clip_to_mazes(10)
+		cap.cam.stop()
+		cap.set_bounding_box(x,y,w,h)
+		cap.cam.start()
+		im,ts = cap.capture_frame()
+		cv2.namedWindow('clipped mazes', cv2.WINDOW_NORMAL)
+		img = ymg.diagnostic_image(im)
+		cv2.imshow('clipped mazes', img)
+
+
+		key = cv2.waitKey(1) & 0xFF
+		if key == ord('q'):
+			quit()
+		response = input("Are you satisfied with the maze locations? (yes/no)")
+		if response == "yes":
+			break
+	#cv2.destroyAllWindows()
+print ("creating maze dispatcher")
 md = MazeDispatcher(ymg)
 
+print ("capturing first frame")
+im, ts = cap.capture_frame()
 frame_num = 0
 t0 = ts
 tt = None
 
 while True:
-	im,ts = cap.capture_frame()
-	frame_num += 1
 	frame_time = ts - t0
-
 	print(f"frame: {frame_num}, elapsed time: {frame_time}")
-
-
 	#wait for previous frame to finish processing
 	if tt is not None:
 		for t in tt:
@@ -83,3 +92,7 @@ while True:
 	key= cv2.waitKey(1) & 0xFF
 	if key == ord('q'):
 		break
+	im,ts = cap.capture_frame()
+	frame_num += 1
+
+
