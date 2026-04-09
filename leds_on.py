96ec10453225854888c2dc456b10ad96947f09c5
Marc Gershow <gershow@gmail.com>
2026-04-09 12:42:38 -0400

    Update leds_on.py

--- leds_on.py
+++ leds_on.py
@@ -3,6 +3,6 @@ from lightcontroller import LightController
 lc = LightController()
 lc.test_leds(0.1)
 for m in range(27):
-    lc.set_led_direct(m,255,255,255,100)
+    lc.set_led_direct(m,255,0,0,100)
 
 lc.update_leds()
