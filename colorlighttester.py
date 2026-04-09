96a37afe3973007ed58ce9c156704e9257cd32b2
Marc Gershow <gershow@gmail.com>
2026-04-09 12:18:43 -0400

    Update colorlighttester.py

--- colorlighttester.py
+++ colorlighttester.py
@@ -78,7 +78,7 @@ winname = 'led correspondence test - remove filter'
 cv2.namedWindow(winname, cv2.WINDOW_NORMAL)
 cv2.resizeWindow(winname, (960,720))
 bright = 100
-light_controller.set_global_brightness(5)
+light_controller.set_global_brightness(9)
 for led in range(27):
 	print(f"setting led {led}")
 	light_controller.set_led_direct(led, bright, bright, bright)
