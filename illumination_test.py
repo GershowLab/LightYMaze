1bf1c1063e43082f80f341e6de0766e8e69c3133
Marc Gershow <gershow@gmail.com>
2026-06-05 15:35:35 -0400

    Update illumination_test.py

--- illumination_test.py
+++ illumination_test.py
@@ -3,11 +3,14 @@ from livetracker import LiveTracker
 lt = LiveTracker()
 lt.experiment_duration = 300
 lt.brightness = 9
-lt.focus(aruco = True)
-lt.calibrate_mazes_aruco()
-while True:
-    if lt.verify_mazes():
-        break
-    lt.calibrate_mazes()
+w = lt.cap.w
+h = lt.cap.h
+lt.cap.set_bounding_box_from_im_coordinates((w-h)/2,0,h,h)
+# lt.focus(aruco = True)
+# lt.calibrate_mazes_aruco()
+# while True:
+#     if lt.verify_mazes():
+#         break
+#     lt.calibrate_mazes()
 lt.focus()
 lt.illumination_response_test()
